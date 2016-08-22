4e21248da3daa76d454c27e54f5695db784b3704
LSaldyt <lucassaldyt@yahoo.com>
2016-08-21 22:00:41 -0600

    Improves resource loading

--- src/eris.py
+++ src/eris.py
@@ -7,16 +7,11 @@ from board  import Board
 from player import Player
 
 if __name__ == '__main__':
-    try: 
-        texture = sf.Texture.from_file("../resources/ship.png")
-    except IOError as e: 
-        print(e)
-        exit(1)
-
-    player = Player(texture)
-
     resources = load()
+
+    player    = Player(resources.textures['ship'])
     board     = Board(resources, 'Eris', 1200, 1200)
+
     player.move((board.window.width/2, board.window.height/2))
     board.add_entity(player)
     board.run()
--- src/load.py
+++ src/load.py
@@ -1,4 +1,28 @@
 import sf
+import os
 
-def load():
-    return {}
+from collections import namedtuple
+
+Resources = namedtuple('Resources', ['textures'])
+
+def load_texture(filepath):
+    try: 
+        return sf.Texture.from_file(filepath)
+    except IOError as e: 
+        print(e)
+        exit(1)
+
+def load(directory='../resources/'):
+
+    textureDict = {}
+
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+        basename = os.path.basename(dirpath)
+        if basename == 'textures':
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                noext    = os.path.splitext(filename)[0]
+                textureDict[noext] = load_texture(filepath)
+
+    return Resources(textureDict)
+
